9579d394be8c2c3555f62cc5e0420de80cd95492
Manoel <mlimeira@ic.uff.br>
2025-12-09 11:14:55 -0500

    Métodos

--- introducao/app.py
+++ introducao/app.py
@@ -1,18 +1,40 @@
 from conta import Conta
 
-conta1 = Conta('1','Limeira',150.5, 0)
+conta1 = Conta('1','Limeira',150.5, 10)
 print(type(conta1))
 print(id(conta1))
 conta2 = Conta(2,'Kaua',5678, 1000)
 print(id(conta2))
+conta3 = conta1
+print(id(conta3))
 #definir características em tempo de execução não é correto
 # conta1.titular = 'Limeira'
 # conta1.saldo = 150.5
 # conta1.numero = 1
 print(conta1.titular)
-print(conta1.saldo)
+#print(conta1.saldo)
 # conta2.titular = 'Kaua'
 # conta2.saldo = 5678
 # conta2.numero = 2
 #conta2.limite = 1000
-print(conta2.limite)
+#print(conta2.limite)
+conta1.ver_saldo()
+conta1.depositar(500)
+conta1.ver_saldo()
+if conta1.sacar(661):
+    print('Saque efetuado com sucesso')
+else:
+    print('Saldo insuficiente')
+
+teste = conta1.ver_saldo()
+print(teste)
+conta3.depositar(100)
+conta1.ver_saldo()
+if conta1.transferir(conta2, 760.5):
+    print('Transferência realizada com sucesso')
+else:
+    print('Saldo insuficiente')
+conta2.ver_saldo()
+print(conta2.__dict__)
+print(dir(Conta))
+print(conta2)
--- introducao/conta.py
+++ introducao/conta.py
@@ -1,7 +1,38 @@
 class Conta:
     #Método de inicialização dos objetos do tipo Conta
+    #Características de identificação da conta
     def __init__(self, numero:int, titular, saldo, limite):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+    #Características comportamentais (o que podemos executar)
+    def sacar(self, valor):
+        if valor > self.saldo + self.limite:
+            return False
+        else:
+            self.saldo -= valor
+            return True
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def ver_saldo(self):
+        if self.saldo < 0:
+            print(f'Conta número: {self.numero} saldo negativo: {self.saldo}, '
+                  f'usando limite de'
+                  f' {self.limite} ainda pode utilizar'
+                  f' {self.limite + self.saldo}')
+        else:
+            print(f'Conta número: {self.numero}, saldo: {self.saldo}')
+
+    #Tranferir
+    def transferir(self, conta_destino, valor):
+        if self.sacar(valor):
+            conta_destino.depositar(valor)
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return f'Conta número: {self.numero}, Titular: {self.titular}'
